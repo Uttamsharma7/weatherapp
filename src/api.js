f3e62bd7f14b2267ec64aa60716a7e988ba1c693
Uttamsharma7 <sharmashivam7087@gmail.com>
2024-07-08 20:19:29 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+ export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '224ed1ff6amsha32b814124a24f6p1b76c4jsn6f5ef0a950a8',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+
+ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+ export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "000fd80cc2603c6ded94b9d4e96bb5c3"; 
